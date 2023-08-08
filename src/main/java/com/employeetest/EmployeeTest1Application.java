fb07d75e6bdde6028bb0243c7bffec90cd9e7f78
Stephan Bastyav Fernandes <stephan@sti.com>
2023-08-08 16:04:51 +0530

    Add existing project files to Git1

---
+++ src/main/java/com/employeetest/EmployeeTest1Application.java
@@ -0,0 +1,13 @@
+package com.employeetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeetest/EmployeeTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
